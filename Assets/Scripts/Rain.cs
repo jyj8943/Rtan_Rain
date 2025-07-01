ff43620a690a5166fee4e8dabcadfeb424b40812
jyj8943 <jyj8943@gmail.com>
2025-07-01 11:32:16 +0900

    숙제
    점수가 감점되는 빨간 비 만들기

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -20,7 +20,7 @@ public class Rain : MonoBehaviour
 
         transform.position = new Vector3(x, y, 0);
 
-        int type = Random.Range(1, 4);
+        int type = Random.Range(1, 5);
         if (type == 1)
         {
             size = 0.8f;
@@ -38,6 +38,12 @@ public class Rain : MonoBehaviour
             size = 1.2f;
             score = 3;
             renderer.color = new Color(150 / 255f, 150 / 255f, 1f, 1f);
+        }
+        else if (type == 4)
+        {
+            size = 0.8f;
+            score = -5;
+            renderer.color = new Color(255 / 255f, 100 / 255f, 255 / 255f, 1f);
         }
 
         transform.localScale = new Vector3(size, size, 0);
